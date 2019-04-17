742f44260e6ac85f9cbc52c865e8fccf95000851
Calvin Choe <calvinchoe1@gmail.com>
2019-04-17 11:07:01 -0400

    Implement first Mysql functionality, separate code better, add todos for planning, begin work on cart methods

--- gocart.go
+++ gocart.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "os"
+  "fmt"
   "database/sql"
 )
 import _ "github.com/go-sql-driver/mysql"
@@ -11,60 +12,91 @@ func main() {
   arg1 := os.Args[1]
   arg2 := os.Args[2]
 
+  print(arg1, arg2);
+
   mysql := MysqlConnection{
-    Host="localhost",
-    Port="3306",
-    User="pantheon",
-    Password="pantheon",
-    Database="pantheon"
+    host: "localhost", // cnf
+    port: "33050", // cnf
+    user: "pantheon", // cnf
+    password: "pantheon", // cnf
+    database: "pantheon", // cnf
+    table: "gocart", // cnf ? maybe just create on initial run?
+    table_index: "CartId",
   }
+  _, err := mysql.Connect();
+
+  if err != nil {
+    print(err);
+  }
+
 
   // @TODO: implement the basic steps to starting a cart
 
 }
 
 type MysqlConnection struct {
-  var Host string;
-  var Port string;
-  var User string;
-  var Password string;
-  var Database string;
+  host string;
+  port string;
+  user string;
+  password string;
+  database string;
   /**
    * The database table which holds data for possible products
    */
-  var table string;
+  table string;
 
   /**
    * The unique field on our table with which we can index our results by
    * - Keep this so we can use it as a type of abstraction for a filter?
    */
-  var table_index string;
+  table_index string;
+
 }
 
 type ConnectionInterface interface {
-  connect() (GoCart, error);
+  Connect() (GoCart, error);
   disconnect() error;
+  GetTable() string;
+}
+
+func (mysql MysqlConnection) GetTable() string {
+  return mysql.table;
 }
 
 /**
  * Any connectors implementing our ConnectionInterface should be able to connect/disconnect to the appropriate persistence layer and return a GoCart instance
+ * @TODO: Rename this func. Separate the insert from the general Connect() functionality--just because we connect to the DB does not mean we want to insert a cart
  */
-func (mysql MysqlConnection) connect() (GoCart, error) {
-  dsn := mysql.User + ":" + mysql.Password + "@" + mysql.Host + "/" + mysql.Database;
+func (mysql MysqlConnection) Connect() (GoCart, error) {
+  dsn := mysql.user + ":" + mysql.password + "@tcp(" + mysql.host + ":" + mysql.port + ")/" + mysql.database + "?charset=utf8";
+  var db *sql.DB;
+  var err error;
 
-  db, err := sql.Open("mysql", dsn);
+  db, err = sql.Open("mysql", dsn);
+
+  defer db.Close();
 
   // @TODO: Create a new record in the database at this point and then pull the record to retrieve the ID
-  cart := GoCart{
-    CartId=123,
-    CartValue=0,
-    Db=db
-  }
+  var id int64;
+  var id_err error;
+  //stmt := `
+  //INSERT INTO ? (CartValue, Items)
+  //VALUES (?, ?)
+  //RETURNING CartId`;
+  //insert, insert_err := db.Prepare(stmt);
+
+  //insert, insert_err := db.Prepare("INSERT INTO ? (CartValue, Items) VALUES (?, ?) RETURNING CartId;");
+  table := mysql.GetTable();
+
+  //stmt := fmt.Sprint("INSERT INTO ", table, " (CartValue, Items) VALUES (", 0.00, ", \"{}\"", ")");
+  //result, query_error := db.Query(stmt);
+
   return cart, err;
 }
 
 func (mysql MysqlConnection) disconnect() error {
 
+  return nil;
 }
 
 
@@ -72,27 +104,54 @@ func (mysql MysqlConnection) disconnect() error {
  * The main struct which holds our application data + config
  */
 
-type GoCart struct {
+type goCart struct {
 
   /**
    * The unique internal Cart ID
    */
-  var CartId uint32;
+  CartId int64;
 
   /**
    * The items that exist in the cart
    */
-  var Items []Item;
+  Items []Item;
 
   /**
    * Recalculated at every addition/subtraction of items from the cart.
    */
-  var CartValue float64;
+  CartValue float64;
 
   /**
    * A db connection
    */
-  var Db sql.DB;
+  Db *sql.DB;
+}
+
+type GoCartInterface interface {
+  new(items []Item, cart_value float64) goCart;
+
+}
+
+func (cart goCart) new(items []Item, cart_value float64) goCart {
+
+  // @TODO: Is this the best way to prepare this statement?
+  result, query_error := db.Exec(fmt.Sprint("INSERT INTO ", table, " (CartValue, Items) VALUES (?, ?)"), "0.00", "{}");
+
+  if id_err != nil {
+    fmt.Println(id_err);
+  }
+
+  if query_error != nil {
+    fmt.Println(query_error);
+  }
+  id, id_err = result.LastInsertId();
+  fmt.Println(id);
+
+  cart := GoCart{
+    CartId: id,
+    CartValue: 0,
+    Db: db,
+  }
 }
 
 /**
@@ -110,31 +169,31 @@ type Item struct {
   /**
    * The unique internal Product ID
    */
-  var ProductId uint32;
+  ProductId int64;
 
   /**
    * Most likely the unique identifier
    */
-  var Sku string;
+  Sku string;
 
   /**
    * The product's name
    */
-  var ProductName string;
+  ProductName string;
 
   /**
    * The product's description
    */
-  var ProductDescription string;
+  ProductDescription string;
 
   /**
    * The product's cost
    */
-  var Cost float64;
+  Cost float64;
 
   /**
    * The product's selling price
    */
-  var Price float64;
+  Price float64;
 
 }
